5cc7af086f6e1cf8cf124a1e88374e9c798663a7
GeorgeManakanatas <manakanatas@gmail.com>
2020-09-22 00:15:56 +0200

    parsing from file
    Storing html contnet to file so I don't spam the site as I try to parse proper

--- web_scraping/my_scraper.py
+++ web_scraping/my_scraper.py
@@ -36,24 +36,49 @@ if my_config.config_values['check_tracked_values']:
             logger.info(page_found)
             #
             if page_found:
+                # parse with BeautifulSoup
                 soup = BeautifulSoup(request_page.content, 'html.parser')
-                # logger.info(str(soup))
-                info = soup.find(id='mw-content-text')
-                entries = soup.find('div', class_='mw-changeslist oo-ui-widget oo-ui-widget-enabled mw-rcfilters-ui-changesListWrapperWidget')
-                # find all h4 elements
-                h4s_list = soup.find_all('h4')
-                div_list = soup.find_all('div')
-                logger.info('there are : %s h4s', str(len(h4s_list)))
-                for h4 in h4s_list:
-                    logger.info(h4)
-                logger.info('there are : %s divs', str(len(div_list)))
-                for div in div_list:
-                    logger.info(div)
+                # create pretty content
+                prettyHTML = soup.prettify("utf-8")
+                # save contents to file to avoid spamming site during dev
+                with open('output/rawHTML.txt', 'wb') as file:
+                    file.write(prettyHTML)
             else:
                 logger.error('page %s not found', entry['url'])
 
         except Exception as exc:
             logger.error('Error getting website url: %s', exc)
+
+# read content from file
+with open('output/rawHTML.txt', 'r') as file:
+    content = file.read()
+# parse with BeautifulSoup
+soup = BeautifulSoup(content, 'html.parser')
+# get content text section
+content_text = soup.find(id='mw-content-text')
+# logger.info('content_text : %s', str(content_text))
+
+# parse the content_text part of the page
+soup = BeautifulSoup(str(content_text), 'html.parser')
+page_options = soup.find_all('span', class_='oo-ui-labelElement-label')
+logger.info('page_options : %s', str(page_options))
+#
+entries = soup.find_all('div', class_='mw-collapsible mw-collapsed mw-enhanced-rc mw-changeslist-line mw-changeslist-edit mw-changeslist-ns3000-Koppeling_IoT-peilsensordata_naar_andere_IoT-_stacks mw-changeslist-line-not-watched mw-made-collapsible')
+logger.info('entries length: %s', str(len(entries)))
+logger.info('entries : %s', str(entries))
+soup = BeautifulSoup(str(entries), 'html.parser')
+# logger.info('entries_soup: %s', str(entries))
+#
+h4s_list = soup.find_all('h4')
+div_list = soup.find_all('div')
+#
+logger.info('there are : %s h4s', str(len(h4s_list)))
+for h4 in h4s_list:
+    logger.info(h4)
+logger.info('there are : %s divs', str(len(div_list)))
+for div in div_list:
+    logger.info(div)
+
         
 
             
